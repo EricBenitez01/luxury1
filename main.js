01dd80744445c4d285deaa5adf1955066acee0ab
EricBenitez01 <leonetkd@gmail.com>
2022-04-30 17:13:36 -0300

    first

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 400 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 400 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 400 });
