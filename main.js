d7e097672eee6360e894968e084ca6ecb3c0687c
EricBenitez01 <leonetkd@gmail.com>
2022-05-01 21:07:04 -0300

    scroll

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', { delay: 400 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 400 });
 ScrollReveal().reveal('.cards-banner-two', { delay: 400 });
+ScrollReveal().reveal('.f', { delay: 350 });
+ScrollReveal().reveal('.t', { delay: 650 });
+ScrollReveal().reveal('.l', { delay: 850 });
